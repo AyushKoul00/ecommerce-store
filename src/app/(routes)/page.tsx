082e908d01d135b3a86e51586bf5c7a26eb61f7a
AyushKoul00 <ayushkoul00@gmail.com>
2024-04-26 18:08:02 -0700

    removed billboard from store page

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -3,7 +3,7 @@ import getProducts from "@/actions/get-products";
 import ProductList from "@/components/product-list";
 import Billboard from "@/components/ui/billboard";
 import Container from "@/components/ui/container";
-  
+
 export const revalidate = 0;
 const storeID = process.env.NEXT_PUBLIC_API_URL!.split('/').pop();
 const HomePage = async () => {
@@ -13,7 +13,7 @@ const HomePage = async () => {
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={billboard} />
+        {/* <Billboard data={billboard} /> */}
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
         </div>
